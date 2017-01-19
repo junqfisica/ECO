9baee2474a999faa4eafac81a2c27ec1207fea59
Carlos Braga <CASBraga@gmail.com>
2017-01-19 20:57:15 -0200

    Added UMotionTrackSaveSystem class and provided testing for it in the main level blueprint

---
+++ Source/ElementalCombat/MotionTrackSaveSystem.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/SaveGame.h"
+#include "MotionTrackSaveSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class ELEMENTALCOMBAT_API UMotionTrackSaveSystem : public USaveGame
+{
+	GENERATED_BODY()
+	
+public:
+	// Public methods
+	// See if the file exists
+	UFUNCTION(BlueprintCallable, Category = "Save Game")
+	static bool DoesFileExist(const FString Dir, const FString SaveFile);
+	// Create Save File
+	UFUNCTION(BlueprintCallable, Category = "Save Game")
+	static bool CreateSaveFile(const FString Dir, const FString SaveFile);
+	// Save Game State after serialization
+	UFUNCTION(BlueprintCallable, Category = "Save Game")
+	static bool SaveGameState(USaveGame* SaveGameObject, const FString Dir, const FString SaveFile);
+	// Load Game State after deserialization
+	UFUNCTION(BlueprintCallable, Category = "Save Game")
+	static USaveGame* LoadGameState(const FString Dir, const FString SaveFile);	
+};
