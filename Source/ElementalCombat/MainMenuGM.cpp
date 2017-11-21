f511178f9295e2e4c1d1e9ed518b18efa7b9ea5c
Carlos Braga <CASBraga@gmail.com>
2017-11-20 22:42:23 -0200

    Carlos - Reconnection bug is fexed but testing my changes would be a good thing (I already tested but one never knows). See Carlos_ChangeLog for details.

--- Source/ElementalCombat/MainMenuGM.cpp
+++ Source/ElementalCombat/MainMenuGM.cpp
@@ -3,6 +3,31 @@
 #include "ElementalCombat.h"
 #include "MainMenuGM.h"
 
+void AMainMenuGM::Logout(AController * Exiting)
+{
+	if (Exiting)
+	{
+		/// In a listen server, we do nothing besides calling Super
+		/*if (!Exiting->IsLocalPlayerController())
+		{
+			/// Return the match state machine to the beginning of the match
+			SetMatchState(MatchState::WaitingToStart);
+		}*/
 
+		/// Call Super to handle the rest of the logout procedures
+		Super::Logout(Exiting);
+	}
+}
 
+void AMainMenuGM::BP_SetMatchState(FName NewState)
+{
+	/// This boolean protects the state machine from changes to leaving or entering map
+	bool bIsValidState = NewState != MatchState::EnteringMap && NewState != MatchState::LeavingMap;
+
+	/// Only when we know it is safe to change the state do e change to the new one
+	if (bIsValidState && bCanChangeFromThisState)
+	{
+		SetMatchState(NewState);
+	}
+}
 
--- Source/ElementalCombat/MainMenuGM.h
+++ Source/ElementalCombat/MainMenuGM.h
@@ -3,18 +3,39 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "MainMenuGM.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class ELEMENTALCOMBAT_API AMainMenuGM : public AGameModeBase
+class ELEMENTALCOMBAT_API AMainMenuGM : public AGameMode
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+	/// Methods
+	/**
+	*   Function overrides Logout function from AGameMode - handles any 
+	*   operations when a player leaves the match
+	*
+	*   @param		Exitting		The controller leaving the match 
+	*/
+	virtual void Logout(AController* Exiting) override;
+
+protected:
+	/// Members
+	/** Boolean is set to true only for states that are acceptable to change from */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game")
+	bool bCanChangeFromThisState;
+
+	/// Methods
+	/**
+	*   Function wrapper for SetMatchState - used in BPs
+	*
+	*   @param		NewState		The new state the match should transition to
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void BP_SetMatchState(FName NewState);
+
 };
